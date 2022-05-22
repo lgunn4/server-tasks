3c9ff15184f37d54e9d82b06480badfc5aeb311a
Logan Gunn <lgunn4@uwo.ca>
2022-05-22 00:10:48 -0400

    using exif tool

--- sort.py
+++ sort.py
@@ -1,4 +1,4 @@
-import os, datetime, errno, argparse, sys
+import os, datetime, errno, argparse, sys, exifread
 
 BASE_EXTENSIONS = ['GIF', 'jpeg', 'JPEG', 'JPG', 'mov', 'MOV', 'mp4', 'MP4', 'PNG', 'png', 'WEBP']
 
@@ -8,8 +8,8 @@ def create_file_list(ext, source):
     files_with_mtime = []
     for filename in [f for f in os.listdir(source) if os.path.splitext(f)[1] in ext]:
         filename_with_path = os.path.join(source, filename)
-        year = datetime.datetime.fromtimestamp(os.stat(filename_with_path).st_mtime).strftime('%Y')
-        month = datetime.datetime.fromtimestamp(os.stat(filename_with_path).st_mtime).strftime('%m')
+        [year, month] = get_year_month_taken(filename_with_path)
+
         files_with_mtime.append((filename, year, month))
     return files_with_mtime
 
@@ -45,6 +45,24 @@ def sort(extensions, source, destination):
     files = create_file_list(ext, source)
     create_directories(files, destination)
     print("Moved %i files" % move_files_to_folders(files, source, destination))
+
+def get_year_month_taken(file_name):
+    with open(file_name, 'rb') as fh:
+        tags = exifread.process_file(fh, stop_tag="EXIF DateTimeOriginal")
+
+        if "EXIF DateTimeOriginal" in tags:
+            date_taken_tag = tags["EXIF DateTimeOriginal"]
+            date_taken_timestamp = datetime.datetime.strptime(str(date_taken_tag), '%Y:%m:%d %H:%M:%S')
+            year = date_taken_timestamp.strftime('%Y')
+            month = date_taken_timestamp.strftime('%m')
+
+            return (year, month)
+    
+        date_taken = os.stat(file_name).st_mtime
+        year = datetime.datetime.fromtimestamp(date_taken).strftime('%Y')
+        month = datetime.datetime.fromtimestamp(date_taken).strftime('%m')
+
+        return (year, month)
 
 
 if __name__ == '__main__':
